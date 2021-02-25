61818d2c72c9a608a49d4a1f24bf49ecb873a934
Alberth Florêncio <alberth@insightlab.ufc.br>
2021-02-25 14:55:41 -0300

    Adição da 01, melhorias em 02 e 03.

--- 03_airflow.py
+++ 03_airflow.py
@@ -1,13 +1,18 @@
 import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
+
+# Lib com o conjunto de scripts
+from meus_scripts import download_bases, unzip_bases, criar_atracacao_fato, criar_carga_fato
 
 # Definindo alguns argumentos básicos
 default_args = {
    'owner': 'fiec',
    'start_date': datetime(2020, 2, 1),
    'retries': 0,
+   'depends_on_past': True,
    }
 
 # Criação do objeto DAG
@@ -18,36 +23,38 @@ with DAG(
 
 # Definições de DAGs
 
-t0 = BashOperator(
+t01 = BashOperator(
     task_id= 'alertas',
-    bash_command="./scripts/alertas.sh",
-    dag=dag
+    bash_command="./scripts/alerta_download.sh"
 )
 
-t1 = BashOperator(
-    task_id= 'download_dados',
-    bash_command="./scripts/download_dados.sh",
-    dag=dag
-) 
+t02 = BashOperator(
+    task_id= 'alertas',
+    bash_command="./scripts/alerta_unzip.sh"
+)
 
-t2 = BashOperator(
-    task_id= 'tratamento_dados',
-    bash_command="./scripts/tratamento_dados.sh",
-    dag=dag
-) 
+t03 = BashOperator(
+    task_id= 'alertas',
+    bash_command="./scripts/alerta_atracacao_fato.sh"
+)
 
-t3 = BashOperator(
-    task_id= 'criar_atracacao_fato',
-    bash_command="python /scripts/criar_atracacao_fato.py",
-    dag=dag
-) 
+t04 = BashOperator(
+    task_id= 'alertas',
+    bash_command="./scripts/alerta_carga_fato.sh"
+)
 
-t4 = BashOperator(
-    task_id= 'criar_carga_fato',
-    bash_command="python /scripts/criar_carga_fato.py",
-    dag=dag
-) 
+t1 = PythonOperator(task_id='download_dados',
+                    python_callable=download_bases)
+
+t2 = PythonOperator(task_id='tratamento_dados',
+                    python_callable=unzip_bases)
+
+t3 = PythonOperator(task_id='criar_atracacao_fato',
+                    python_callable=criar_atracacao_fato)
+
+t4 = PythonOperator(task_id='criar_carga_fato',
+                    python_callable=criar_carga_fato)
 
 # Cada operação informa ao script de alertas se a operação anterior ocorreu bem
 # Em caso de erros, o script gera alertas pré definidos
-t1 >> t0 >> t2 >> t0 >> t3 >> t0 >> t4
+t1 >> t01 >> t2 >> t02 >> t3 >> t03 >> t4 >> t04
